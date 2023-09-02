5f02c66d61543ab6bfcf5a0b89013d47d0050f9a
Aryan9rana <ranaryan9999@gmail.com>
2023-09-02 19:19:00 +0530

    changes reversed

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 const pool = mysql.createPool({
 connectionLimit : 100,
-host            : process.env.DB_HOST || "localhost",
-user            : process.env.DB_USER || "root",
-password        : process.env.DB_PASS || "root",
-database        : process.env.DB_NAME || "blood_donation"
+host            : process.env.DB_HOST ,
+user            : process.env.DB_USER,
+password        : process.env.DB_PASS,
+database        : process.env.DB_NAME
 });
 
 
